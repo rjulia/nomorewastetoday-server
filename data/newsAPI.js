8d1de0cb3c64f58644a79536c820427c65795875
Ramon Julia <ramon@tofugear.com>
2020-05-15 14:00:04 +0800

    add one more news

--- data/newsAPI.js
+++ data/newsAPI.js
@@ -7,7 +7,7 @@ class NewsAPI extends RESTDataSource {
     this.baseURL = process.env.APP_API;
   }
 
-  async getNewsInfo(numberNews = 4) {
+  async getNewsInfo(numberNews = 5) {
     try {
       const data = await this.get(
         `?sources=&apiKey=${process.env.APP_API_KEY}&pageSize=${numberNews}&page=1&q=recycling, hong kong`
